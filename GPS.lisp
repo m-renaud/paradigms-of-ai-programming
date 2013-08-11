d03ef800802b2668f84a42229699bd0ae53a785b
Matt Renaud <mrenaud92@gmail.com>
2013-08-11 16:00:08 -0400

    Add 'monkey and bananas', 'maze', and 'block world'.

--- GPS.lisp
+++ GPS.lisp
@@ -1,17 +1,6 @@
->;;; GeneralProblem Solver
+;;; GeneralProblem Solver
 ;;; Chapter 4
 
-
-;; Section 3.19 pg. 101
-(defun find-all (item sequence &rest keyword-args
-                      &key (test #'eql) test-not &allow-other-keys)
-  "Find all those elements of sequenc that match item,
-  according to the keywords. Doesn't alter seuqence."
-  (if test-not
-      (apply #'remove item sequence
-             :test-not (complement test-not) keyword-args)
-    (apply #'remove item sequence
-           :test (complement test) keyword-args)))
 
 ;; Section 4.11 pg. 129
 ;; Originally member-equal
@@ -38,10 +27,6 @@
   "Is x of the form: (executing ...) ?"
   (starts-with x 'executing))
 
-(defun starts-with (list x)
-  "Is this a list whole first element is x?"
-  (and (consp list) (eql (first list) x)))
-
 (defun convert-op (op)
   "Make op conform to the (EXECUTING op) convention."
   (unless (some #'executing-p (op-add-list op))
@@ -63,8 +48,11 @@
   ;; Therefore, the problem is not solved with this approarch.
   ;; (let ((state (set-difference state goals)))
 
-  (remove-if #'atom (achieve-all (cons '(start) state) goals nil)))
+  (find-all-if #'action-p (achieve-all (cons '(start) state) goals nil)))
 
+(defun action-p (x)
+  "Is x something that is (start) or (executing ...)?"
+  (or (equal x '(start)) (executing-p x)))
 
 (defun achieve (state goal goal-stack)
   "A goal is achieved if it already holds, or if there is
@@ -130,7 +118,8 @@
                     (set-difference *dbg-ids* ids))))
 
 
-
+;; =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; Section 4.4 - Test
 (defparameter *school-ops*
   (list
    (make-op :action 'drive-son-to-school
@@ -160,34 +149,119 @@
 (mapc #'convert-op *school-ops*)
 
 ;; Section 4.4 - Stage 4: Test
-(gps '(son-at-home car-needs-battery have-money have-phone-book)
-     '(son-at-school)
-     *school-ops*)
+;; (gps '(son-at-home car-needs-battery have-money have-phone-book)
+;;      '(son-at-school)
+;;      *school-ops*)
 
 
 ;; Section 4.7 - The Clobbered Sibling Goal Problem
 ;;  With the original version of GPS without achieve all, have-money
 ;;  is achieved and then son-at-school is achieved, but in the process,
 ;;  undoing the goal of have-money.
-(gps '(son-at-home car-needs-battery have-money have-phone-book)
-     '(have-money son-at-school)
-     *school-ops*)
+;; (gps '(son-at-home car-needs-battery have-money have-phone-book)
+;;      '(have-money son-at-school)
+;;      *school-ops*)
 
 
 ;; Section 4.8 - Leaping Before You Look Problem
 ;;   Could be solved by doing a (set-differenc *state* goals)
 ;;   before running GPS. See GPS definition for explatation.
-(gps '(son-at-home car-needs-battery have-money have-phone-book)
-     '(son-at-school have-money)
-     *school-ops*)
+;; (gps '(son-at-home car-needs-battery have-money have-phone-book)
+;;      '(son-at-school have-money)
+;;      *school-ops*)
 
 
 ;; Section 4.9 - The Recursive Subgoal Problem
-(gps '(son-at-home car-needs-battery have-money)
-     '(son-at-school)
-     *school-ops*)
+;; (gps '(son-at-home car-needs-battery have-money)
+;;      '(son-at-school)
+;;      *school-ops*)
 
 
 
+;; =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ;; Section 4.12 - The New Domain Problem: Monkey and Bananas
+(defparameter *banana-ops*
+  (list
+    (op 'climb-on-chair
+        :preconds '(chair-at-middle-room at-middle-room on-floor)
+        :add-list '(at-bananas on-chair)
+        :del-list '(at-middle-room on-floor))
+    (op 'push-chair-from-door-to-middle-room
+        :preconds '(chair-at-door at-door)
+        :add-list '(chair-at-middle-room at-middle-room)
+        :del-list '(chair-at-door at-door))
+    (op 'walk-from-door-to-middle-room
+        :preconds '(at-door on-floor)
+        :add-list '(at-middle-room)
+        :del-list '(at-door))
+    (op 'grasp-bananas
+        :preconds '(at-bananas empty-handed)
+        :add-list '(has-bananas)
+        :del-list '(empty-handed))
+    (op 'drop-ball
+        :preconds '(has-ball)
+        :add-list '(empty-handed)
+        :del-list '(has-ball))
+    (op 'eat-bananas
+        :preconds '(has-bananas)
+        :add-list '(empty-handed not-hungry)
+        :del-list '(has-bananas hungry))))
+
+(mapc #'convert-op *banana-ops*)
+
+
+;; Pg 133
+;; (gps '(at-door on-floor has-ball hungry chair-at-door) '(not-hungry))
+
+
+;; =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; Section 4.13 - The Maze Searching Domain
+(defun make-maze-ops (pair)
+  "Make maze ops in both directions."
+  (list (make-maze-op (first pair) (second pair))
+        (make-maze-op (second pair) (first pair))))
+
+(defun make-maze-op (here there)
+  "Make an operator to move between two places in a maze."
+  (op `(move from ,here to ,there)
+      :preconds `((at ,here))
+      :add-list `((at ,there))
+      :del-list `((at ,here))))
+
+(defparameter *maze-ops*
+  (mappend #'make-maze-ops
+     '((1 2) (2 3) (3 4) (4 9) (9 14) (9 8) (8 7) (7 12) (12 13)
+       (12 11) (11 6) (11 16) (16 17) (17 22) (21 22) (22 23)
+       (23 18) (23 24) (24 19) (19 20) (20 15) (15 10) (10 5) (20 25))))
+
+
+;; =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+;; Section 4.14 - The Blocks World Domain
+(defun make-block-ops (blocks)
+  (let ((ops nil))
+    (dolist (a blocks)
+      (dolist (b blocks)
+        (unless (equal a b)
+          (dolist (c blocks)
+            (unless (or (equal c a) (equal c b))
+              (push (move-op a b c) ops)))
+          (push (move-op a 'table b) ops)
+          (push (move-op a b 'table) ops))))
+    ops))
+
+(defun move-op (a b c)
+  "Make an operator to move A from B to C."
+  (op `(move ,a from ,b to ,c)
+      :preconds `((space on ,a) (space on ,c) (,a on ,b))
+      :add-list (move-ons a b c)
+      :del-list (move-ons a c b)))
+
+(defun move-ons (a b c)
+  (if (eq b 'table)
+      `((,a on ,c))
+    `((,a on ,c) (space on ,b))))
+    
+;; (gps '((a on b) (b on c) (c on table) (space on a) (space on table))
+;;	      '((b on a) (c on b)))
+
 
